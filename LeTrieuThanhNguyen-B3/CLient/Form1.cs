deef599409e619a7f9cd95107f029d4efe4d370b
nguyenle510 <trieunguyen30@gmail.com>
2017-10-13 20:35:18 +0700

    commit 1

---
+++ LeTrieuThanhNguyen-B3/CLient/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace CLient
+{
+    public partial class Form1 : Form
+    {
+        Socket server;
+        IPEndPoint ipep;
+        EndPoint remote;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+        }
+        
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = "Keo";
+            int a = 0;
+            byte[] send = BitConverter.GetBytes(a);
+            server.SendTo(send, (EndPoint)ipep);
+
+            byte[] rec = new byte[20];
+            remote = (EndPoint)ipep;
+            server.ReceiveFrom(rec, ref remote);
+            textBox2.Text = Encoding.ASCII.GetString(rec);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Bao";
+            int a = 0;
+            byte[] send = BitConverter.GetBytes(a);
+            server.SendTo(send, (EndPoint)ipep);
+
+            byte[] rec = new byte[20];
+            remote = (EndPoint)ipep;
+            server.ReceiveFrom(rec, ref remote);
+            textBox2.Text = Encoding.ASCII.GetString(rec);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Bua";
+            int a = 0;
+            byte[] send = BitConverter.GetBytes(a);
+            server.SendTo(send, (EndPoint)ipep);
+
+            byte[] rec = new byte[20];
+            remote = (EndPoint)ipep;
+            server.ReceiveFrom(rec, ref remote);
+            textBox2.Text = Encoding.ASCII.GetString(rec);
+        }
+
+    }
+}
---
+++ LeTrieuThanhNguyen-B3/Server/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace Server
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            int recv;
+            byte[] data = new byte[4];
+
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 9050);
+            Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            newsock.Bind(ipep);
+
+            Console.WriteLine("Waiting for a client...");
+
+            IPEndPoint ipClient = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)(ipClient);
+
+            recv = newsock.ReceiveFrom(data, ref Remote);
+            int clientChoosen = BitConverter.ToInt32(data, 0);
+
+            Random rand = new Random();
+            int serverChoosen = rand.Next(0, 2);
+
+            if (clientChoosen == serverChoosen)
+            {
+                byte[] result = Encoding.ASCII.GetBytes("Hoa");
+                newsock.SendTo(result, Remote);
+            }
+
+        }
+    }
+}
